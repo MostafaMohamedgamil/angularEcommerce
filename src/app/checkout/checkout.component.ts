9d6b49ebb588644ac87c4812d34011de11e08b91
hanyganal72 <hany.gamal290@gmail.com>
2024-09-17 12:45:08 +0300

    fix issue in checkout

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -2,54 +2,45 @@ import { Component } from '@angular/core';
 import { CartService } from './../shared/services/cart.service';
 import { FormControl, FormGroup } from '@angular/forms';
 
-
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.scss'
+  styleUrl: './checkout.component.scss',
 })
 export class CheckoutComponent {
-
   cartDetails: any = null;
 
-  constructor(private _CartService: CartService) { }
-
+  constructor(private _CartService: CartService) {}
 
   ngOnInit(): void {
-
-
     this._CartService.getLoggedUserCart().subscribe({
       next: (res) => {
-        this.cartDetails = res.data
+        this.cartDetails = res.data;
         console.log(res);
-
       },
       error: (err) => {
         console.log(err);
-
-      }
-    })
+      },
+    });
   }
 
-  shippingAddress: FormGroup = new FormGroup({
+  form: FormGroup = new FormGroup({
     details: new FormControl(null),
     phone: new FormControl(null),
-    city: new FormControl(null)
-  })
+    city: new FormControl(null),
+  });
 
-  handelSubmit(shippingAddress: FormGroup) {
-    console.log(shippingAddress.value);
-    this._CartService.onlinePayment(shippingAddress, "66d6dc14832c8800a83eaee9").subscribe({
-      next: (res: any) => {
-        console.log("res.session.url", res.session.url);
-
-      },
-      error: (err) => {
-        console.log(err);
-      }
-
-    })
-
+  handelSubmit() {
+    // return console.log(shippingAddress.value);
+    this._CartService
+      .onlinePayment(this.form.value, '66d6dc14832c8800a83eaee9')
+      .subscribe({
+        next: (res: any) => {
+          console.log('res.session.url', res.session.url);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
   }
-
 }
